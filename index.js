12e03662d02782fafb98c294d19404622a6563ee
Juan Ortiz-Acosta <jdoa2003@gmail.com>
2023-08-04 10:21:10 -0600

    Finished Lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution here
+
+function findMatching(arrayOfDriverNames, nameOfDriver) {
+    return arrayOfDriverNames.filter(name => name.toLowerCase() === nameOfDriver.toLowerCase());
+}
+
+function fuzzyMatch(arrayOfDriverNames, strOfBeginning) {
+    return arrayOfDriverNames.filter(name => {
+        return fuzzyMatchHelper(name, strOfBeginning);
+    });
+}
+
+function matchName(arrayOfDriverObj, nameOfDriver) {
+    return arrayOfDriverObj.filter(obj => obj[`name`] === nameOfDriver);
+}
+
+function fuzzyMatchHelper(name, strOfBeginning){
+    const lng = strOfBeginning.length;
+    // console.log(`lng: ${lng}`);
+    for (let i = 0; i < lng; i++){
+        // console.log(`i: ${i}`);
+        // console.log(`nameChar: ${strOfBeginning.charAt(i)}`);
+        if (name.charAt(i) !== strOfBeginning.charAt(i)) {
+            // console.log(`false`)
+            return false;
+        }
+    }
+    // console.log(`true`)
+    return true;
+}
+
+// fuzzyMatchHelper(`Juan`, `Ja`);
